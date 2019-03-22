a07d21f2f3962223e1f42c2c92f05d2a49d17f89
Ruggero Turra <ruggero.turra@cern.ch>
2019-03-22 14:01:07 +0100

    move to python3

--- ROOTDiff.py
+++ ROOTDiff.py
@@ -21,15 +21,15 @@ def abs_name(obj):
 def diff_axis(axis1, axis2):
     logging.debug("comparing axis %s and %s", axis1.GetName(), axis2.GetName())
     if (axis1.GetNbins() != axis2.GetNbins()):
-        print "< {} n bins = {}".format(abs_name(axis1), axis1.GetNbins())
-        print "> {} n bins = {}".format(abs_name(axis2), axis2.GetNbins())
+        print("< {} n bins = {}".format(abs_name(axis1), axis1.GetNbins()))
+        print("> {} n bins = {}".format(abs_name(axis2), axis2.GetNbins()))
         return False
-    for ibin in xrange(1, (axis1.GetNbins() + 1)):
+    for ibin in range(1, (axis1.GetNbins() + 1)):
         if axis1.GetBinLowEdge(ibin) != axis2.GetBinLowEdge(ibin):
-            print "< {} different binning (bin {} = {})".format(abs_name(axis1), ibin,
-                                                                axis1.GetBinLowEdge(ibin))
-            print "> {} different binning (bin {} = {})".format(abs_name(axis2), ibin,
-                                                                axis2.GetBinLowEdge(ibin))
+            print("< {} different binning (bin {} = {})".format(abs_name(axis1), ibin,
+                                                                axis1.GetBinLowEdge(ibin)))
+            print("> {} different binning (bin {} = {})".format(abs_name(axis2), ibin,
+                                                                axis2.GetBinLowEdge(ibin)))
             return False
     return True
 
@@ -46,37 +46,37 @@ def diff_graph(graph1, graph2):
         return y
 
     if graph1.GetN() != graph2.GetN():
-        print "< {} npoints = {}".format(graph1.GetN())
-        print "> {} npoints = {}".format(graph2.GetN())
+        print("< {} npoints = {}".format(graph1.GetN()))
+        print("> {} npoints = {}".format(graph2.GetN()))
         return False
     # TODO(sort the points?)
     for ipoint, (x1, y1, x2, y2) in enumerate(zip(get_x(graph1), get_y(graph1),
                                                   get_x(graph2), get_y(graph2))):
         if (x1, y1) != (x2, y2):
-            print "< {} point {} = ({}, {})".format(abs_name(graph1), ipoint, x1, y1)
-            print "> {} point {} = ({}, {})".format(abs_name(graph2), ipoint, x2, y2)
+            print("< {} point {} = ({}, {})".format(abs_name(graph1), ipoint, x1, y1))
+            print("> {} point {} = ({}, {})".format(abs_name(graph2), ipoint, x2, y2))
             return False
 
 
 def diff_graph_asym_errors(graph1, graph2):
     if not diff_graph(graph1, graph2):
         return False
-    for i in xrange(graph1.GetN()):
+    for i in range(graph1.GetN()):
         if graph1.GetErrorXhigh(i) != graph2.GetErrorXhigh(i):
-            print "< {} point {} error x-up = {}".format(abs_name(graph1), i, graph1.GetErrorXhigh(i))
-            print "> {} point {} error x-up = {}".format(abs_name(graph2), i, graph2.GetErrorXhigh(i))
+            print("< {} point {} error x-up = {}".format(abs_name(graph1), i, graph1.GetErrorXhigh(i)))
+            print("> {} point {} error x-up = {}".format(abs_name(graph2), i, graph2.GetErrorXhigh(i)))
             return False
         if graph1.GetErrorXlow(i) != graph2.GetErrorXlow(i):
-            print "< {} point {} error x-low = {}".format(abs_name(graph1), i, graph1.GetErrorXlow(i))
-            print "> {} point {} error x-low = {}".format(abs_name(graph2), i, graph2.GetErrorXlow(i))
+            print("< {} point {} error x-low = {}".format(abs_name(graph1), i, graph1.GetErrorXlow(i)))
+            print("> {} point {} error x-low = {}".format(abs_name(graph2), i, graph2.GetErrorXlow(i)))
             False
         if graph1.GetErrorYhigh(i) != graph2.GetErrorYhigh(i):
-            print "< {} point {} error y-up = {}".format(abs_name(graph1), i, graph1.GetErrorYhigh(i))
-            print "> {} point {} error y-up = {}".format(abs_name(graph2), i, graph2.GetErrorYhigh(i))
+            print("< {} point {} error y-up = {}".format(abs_name(graph1), i, graph1.GetErrorYhigh(i)))
+            print("> {} point {} error y-up = {}".format(abs_name(graph2), i, graph2.GetErrorYhigh(i)))
             return False
         if graph1.GetErrorYlow(i) != graph2.GetErrorYlow(i):
-            print "< {} point {} error y-low = {}".format(abs_name(graph1), i, graph1.GetErrorYlow(i))
-            print "> {} point {} error y-low = {}".format(abs_name(graph2), i, graph2.GetErrorYlow(i))
+            print("< {} point {} error y-low = {}".format(abs_name(graph1), i, graph1.GetErrorYlow(i)))
+            print("> {} point {} error y-low = {}".format(abs_name(graph2), i, graph2.GetErrorYlow(i)))
             False
         return True
 
@@ -91,13 +91,13 @@ def diff_histos(histo1, histo2):
         lines.append("> nbins = {}".format(histo2.GetNbinsX()))
         
         
-    for ibin in xrange(1, (histo1.GetNbinsX() + 1)):
+    for ibin in range(1, (histo1.GetNbinsX() + 1)):
         if histo1.GetBinLowEdge(ibin) != histo2.GetBinLowEdge(ibin):
             lines.append("< different binning (bin {} = {})".format(ibin, histo1.GetBinLowEdge(ibin)))
             lines.append("> different binning (bin {} = {})".format(ibin, histo2.GetBinLowEdge(ibin)))
 
     bins_different = []
-    for ibin in xrange(1, (histo1.GetNbinsX() + 1)):
+    for ibin in range(1, (histo1.GetNbinsX() + 1)):
         if histo1.GetBinContent(ibin) != histo2.GetBinContent(ibin):
             bins_different.append(ibin)
 
@@ -118,7 +118,7 @@ def diff_histos(histo1, histo2):
             lines.append(" other {} different bins".format(len(bins_different) - len(to_write)))
 
     bins_different = []
-    for ibin in xrange(1, (histo1.GetNbinsX() + 1)):
+    for ibin in range(1, (histo1.GetNbinsX() + 1)):
         if histo1.GetBinError(ibin) != histo2.GetBinError(ibin):
             bins_different.append(ibin)
 
@@ -144,35 +144,35 @@ def diff_histos(histo1, histo2):
         lines.append("> different entries ({})".format(histo2.GetEntries()))
 
     if lines:
-        print "< {}".format(abs_name(histo1))
-        print "> {}".format(abs_name(histo2))
-        print '\n'.join(("  " + l for l in lines))
+        print("< {}".format(abs_name(histo1)))
+        print("> {}".format(abs_name(histo2)))
+        print('\n'.join(("  " + l for l in lines)))
 
     return len(lines) > 0
 
 
 def diff_tf1(obj1, obj2):
     if obj1.GetExpFormula() != obj2.GetExpFormula():
-        print "< different formula {}".format(obj1.GetExpFormula())
-        print "> different formula {}".format(obj2.GetExpFormula())
+        print("< different formula {}".format(obj1.GetExpFormula()))
+        print("> different formula {}".format(obj2.GetExpFormula()))
         return False
     if obj1.GetNpar() != obj2.GetNpar():
-        print "< different number of parameters {}".format(obj1.GetNpar)
-        print "> different number of parameters {}".format(obj2.GetNpar)
+        print("< different number of parameters {}".format(obj1.GetNpar))
+        print("> different number of parameters {}".format(obj2.GetNpar))
         return False
 
     result = True
     if obj1.GetMaximumX() != obj2.GetMaximumX() or obj1.GetMinimumX() != obj2.GetMinimumX():
-        print "< different range {} - {}".format(obj1.GetMinimumX(), obj1.GetMaximumX())
-        print "> different range {} - {}".format(obj2.GetMinimumX(), obj2.GetMaximumX())
+        print("< different range {} - {}".format(obj1.GetMinimumX(), obj1.GetMaximumX()))
+        print("> different range {} - {}".format(obj2.GetMinimumX(), obj2.GetMaximumX()))
         result = False
 
-    for ipar in xrange(obj1.GetNpar()):
+    for ipar in range(obj1.GetNpar()):
         par1 = obj1.GetParameter(ipar)
         par2 = obj2.GetParameter(ipar)
         if (par1 != par2):
-            print "< different parameter {} = {}".format(ipar, par1)
-            print "> different parameter {} = {}".format(ipar, par2)
+            print("< different parameter {} = {}".format(ipar, par1))
+            print("> different parameter {} = {}".format(ipar, par2))
             result = False
 
     return result
@@ -188,8 +188,8 @@ def diff_list(list1, list2):
 
 def diff_obj(obj1, obj2):
     if type(obj1) != type(obj2):
-        print "< {} type = {}".format(abs_name(obj1), type(obj1))
-        print "> {} type = {}".format(abs_name(obj2), type(obj2))
+        print("< {} type = {}".format(abs_name(obj1), type(obj1)))
+        print("> {} type = {}".format(abs_name(obj2), type(obj2)))
         return False
 
     if issubclass(type(obj1), ROOT.TDirectory):
@@ -233,11 +233,11 @@ def diff_directory(directory1, directory2, different_names=False):
     d12 = set(keys1) - set(keys2)
     if d12:
         for k in sorted(d12):
-            print "< {}".format(k)
+            print("< {}".format(k))
     d21 = set(keys2) - set(keys1)
     if d21:
         for k in sorted(d21):
-            print "> {}".format(k)
+            print("> {}".format(k))
 
     common_keys = set(keys1).intersection(keys2)
 
@@ -256,10 +256,10 @@ def diff(first_file, second_file):
     f2 = ROOT.TFile.Open(second_file)
 
     if not f1:
-        print "{} do not exists. Exit".format(first_file)
+        print("{} do not exists. Exit".format(first_file))
         return 1
     if not f2:
-        print "{} do not exists. Exit".format(second_file)
+        print("{} do not exists. Exit".format(second_file))
         return 1
 
     return diff_directory(f1, f2, True)
